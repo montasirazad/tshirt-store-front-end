d41dacd2baccf15d256fca8e19bd79883e51cda9
unknown <montasirazad@gmail.com>
2022-05-08 03:19:37 +0600

    find one problem solved

--- src/components/BuyItem/BuyItem.js
+++ src/components/BuyItem/BuyItem.js
@@ -2,25 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const BuyItem = () => {
-    const [service, setService] = useState({});
+    const [item, setItem] = useState({});
     const { id } = useParams();
 
 
-    // useEffect(() => {
-    //     const url = `http://localhost:5000/all-products/${id}`
-    //     console.log(url);
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data);
-    //             // setService(data)
-    //         })
+    useEffect(() => {
+        const url = `http://localhost:5000/all-products/${id}`
+        console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setItem(data)
+            })
 
-    // }, [id]);
-    
+    }, [id]);
+
     return (
         <div>
             <p>You choose product id {id} </p>
+            <p>name: {item.name}</p>
         </div>
     );
 };
